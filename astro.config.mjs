825a64aa183acf3690559446df22db10985d7bc5
TheBlockiest <theblockiest@gmail.com>
2025-07-21 16:30:45 -0400

    Re-added favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,6 +14,7 @@ export default defineConfig({
 			logo: {
 				src: '/src/assets/logo_simple.webp',
 			},
+			favicon: '/src/assets/favicon.ico',
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/theblockiest/xdrv-charting-guide' },
 				{ icon: 'blueSky', label: 'Bluesky', href: 'https://bsky.app/profile/theblockiest.bsky.social' },
 				{ icon: 'external', label: 'Resources', href: '/xdrv-charting-guide/resources' },
